3b6c9d65479d5cfe9616f2ec7362146ba626c4f0
NTI-Akikaze <duybach.224575@gmail.com>
2019-05-01 09:58:30 +0000

    Clean up all pc2team instance.

--- Vault/pc2-full/pc2-9.5.3/samps/contests/sumithello/config/sumit/input_format_validators/valid.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-rem
-rem
-rem
-rem
-echo valid.bat returns exit code 42
-exit 42
--- Vault/pc2-full/pc2-9.5.3/samps/src/hello.rb
+++
@@ -1,9 +0,0 @@
-#
-# File:    hello.rb
-# Purpose: to print hello world in Ruby
-# Author:  pc2@ecs.csus.edu or http://www.ecs.csus.edu/pc2
-#
-
-puts "Hello World.\n";
-
-# eof $Id$
