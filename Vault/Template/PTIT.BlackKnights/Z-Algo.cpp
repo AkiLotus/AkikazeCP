5f25e59d9555de562c8d1ee689fbe603360c85fe
Duy-Bach Le <duybach.224575@gmail.com>
2019-10-06 12:25:04 +0000

    Update Z-Algo.cpp
    Fix a small bug in the code (how the heck did I make it?)

--- Vault/Template/PTIT.BlackKnights/Z-Algo.cpp
+++ Vault/Template/PTIT.BlackKnights/Z-Algo.cpp
@@ -1,7 +1,7 @@
 vector<int> Z_Algo(string S) {
 	vector<int> z(S.size()); int x = 0, y = 0;
 	for (int i=1; i<S.size(); i++) {
-		z[i] = max(0LL, min(z[i-x], y-i+1));
+		z[i] = max(0, min(z[i-x], y-i+1));
 		while (i+z[i] < S.size() && S[z[i]] == S[i+z[i]]) {
 			x = i; y = i + z[i]; z[i]++;
 		}
