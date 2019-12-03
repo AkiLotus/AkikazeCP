733aeca9c578d1a1dced0daf8217fbcaa5ddb57c
Duy-Bach Le <41355100+NTI-Akikaze@users.noreply.github.com>
2019-12-03 17:35:56 +0700

    Practice session.

---
+++ Vault/Codeforces Problems/1196B.cpp
@@ -0,0 +1,88 @@
+/**********************************************
+*||  Code written by Akikaze                ||*
+*||  Duy-Bach Le, #Team4T's Chief Executor  ||*
+**********************************************/
+
+// File name: 1196B.cpp
+// Time created: Mon Nov 25 2019 16:31
+
+/************** [OPTIMIZATION PROTOCOL] **************/
+#pragma GCC optimize("Ofast")
+/*****************************************************/
+
+/************** [LIBRARY PROTOCOL] **************/
+#include <bits/stdc++.h>
+using namespace std;
+/************************************************/
+
+/************** [GNU OMISSIONS] **************/
+#define y0 withindarkness
+#define y1 apinklotusbloomed
+#define yn carryingapurplesoul
+#define j1 togetherformingtheTeam
+/*********************************************/
+
+/************** [LEGENDS/CONSTANTS] **************/
+#define endl '\n'
+#define i64 long long
+#define ld long double
+const long long Mod = 1000000007LL, INF = 1e9, LINF = 1e18;
+const long double Pi = 3.141592653589793116L;
+const long double EPS = 0.000000001L, Gold = ((1.0L+sqrt(5.0L))/2.0L);
+long long keymod[] = {1000000007LL, 1000000009LL, 1000000021LL, 1000000033LL};
+mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng64(chrono::steady_clock::now().time_since_epoch().count());
+/*************************************************/
+
+/************** [MASTER CONTROLS - PHASE 1] **************/
+int OImode = 0;
+int MultiTest = 1;
+int Interactive = 0;
+
+const char infile[] = "FILE.IN";
+const char outfile[] = "FILE.OUT";
+/*********************************************************/
+
+/************** [GLOBAL VARIABLES] **************/
+int n, k; vector<int> a;
+/************************************************/
+
+/************** [FUNCTIONS] **************/
+
+
+void Input() {
+	cin >> n >> k; a.resize(n);
+	for (auto &z: a) cin >> z;
+}
+
+void Solve() {
+	vector<int> OddEyes;
+	for (int i=0; i<n; i++) {
+		if (a[i] % 2 != 0) OddEyes.push_back(i + 1);
+	}
+	if (k > OddEyes.size()) {cout << "NO\n"; return;}
+	if (OddEyes.size() % 2 != k % 2) {cout << "NO\n"; return;}
+	cout << "YES\n";
+	for (int i=0; i<k-1; i++) cout << OddEyes[i] << " ";
+	cout << n << endl;
+}
+/*****************************************/
+
+/************** [MAIN] **************/
+int main(int argc, char* argv[]) {
+	#ifndef Akikaze
+		if (OImode) {
+			assert(freopen(infile, "r", stdin));
+			assert(freopen(outfile, "w", stdout));
+		}
+	#endif
+	
+	ios_base::sync_with_stdio(false);
+	if (!Interactive) cin.tie(NULL);
+	
+	int T = 1; if (MultiTest) {cin >> T; cin.ignore();}
+	while(T--) {Input(); Solve();}
+	
+	return 0;
+}
+/************************************/
---
+++ Vault/Codeforces Problems/1199A.cpp
@@ -0,0 +1,89 @@
+/**********************************************
+*||  Code written by Akikaze                ||*
+*||  Duy-Bach Le, #Team4T's Chief Executor  ||*
+**********************************************/
+
+// File name: 1199A.cpp
+// Time created: Mon Nov 25 2019 16:23
+
+/************** [OPTIMIZATION PROTOCOL] **************/
+#pragma GCC optimize("Ofast")
+/*****************************************************/
+
+/************** [LIBRARY PROTOCOL] **************/
+#include <bits/stdc++.h>
+using namespace std;
+/************************************************/
+
+/************** [GNU OMISSIONS] **************/
+#define y0 withindarkness
+#define y1 apinklotusbloomed
+#define yn carryingapurplesoul
+#define j1 togetherformingtheTeam
+/*********************************************/
+
+/************** [LEGENDS/CONSTANTS] **************/
+#define endl '\n'
+#define i64 long long
+#define ld long double
+const long long Mod = 1000000007LL, INF = 1e9, LINF = 1e18;
+const long double Pi = 3.141592653589793116L;
+const long double EPS = 0.000000001L, Gold = ((1.0L+sqrt(5.0L))/2.0L);
+long long keymod[] = {1000000007LL, 1000000009LL, 1000000021LL, 1000000033LL};
+mt19937 rng32(chrono::steady_clock::now().time_since_epoch().count());
+mt19937_64 rng64(chrono::steady_clock::now().time_since_epoch().count());
+/*************************************************/
+
+/************** [MASTER CONTROLS - PHASE 1] **************/
+int OImode = 0;
+int MultiTest = 0;
+int Interactive = 0;
+
+const char infile[] = "FILE.IN";
+const char outfile[] = "FILE.OUT";
+/*********************************************************/
+
+/************** [GLOBAL VARIABLES] **************/
+int n, x, y; vector<int> a;
+/************************************************/
+
+/************** [FUNCTIONS] **************/
+
+
+void Input() {
+	cin >> n >> x >> y; a.resize(n);
+	for (auto &z: a) cin >> z;
+}
+
+void Solve() {
+	for (int i=0; i<n; i++) {
+		bool valid = true;
+		for (int j=-x; j<=y; j++) {
+			if (j == 0) continue;
+			if (i+j < 0 || i+j>=n) continue;
+			if (a[i+j] <= a[i]) {valid = false; break;}
+		}
+		if (valid) {cout << (i + 1) << endl; return;}
+	}
+	exit(4);
+}
+/*****************************************/
+
+/************** [MAIN] **************/
+int main(int argc, char* argv[]) {
+	#ifndef Akikaze
+		if (OImode) {
+			assert(freopen(infile, "r", stdin));
+			assert(freopen(outfile, "w", stdout));
+		}
+	#endif
+	
+	ios_base::sync_with_stdio(false);
+	if (!Interactive) cin.tie(NULL);
+	
+	int T = 1; if (MultiTest) {cin >> T; cin.ignore();}
+	while(T--) {Input(); Solve();}
+	
+	return 0;
+}
+/************************************/
